2e2b9f41558ba28db260f04a41ac1c511e5276ac
baobabKoodaa <git23123761293@baobab.fi>
2022-05-26 22:35:26 +0300

    Spirit can answer country and state questions.

--- chatbot.js
+++ chatbot.js
@@ -11,24 +11,39 @@ let questGoals = {
     rage: 3
 }
 
+const countryMappings = {"BD": "Bangladesh", "BE": "Belgium", "BF": "Burkina Faso", "BG": "Bulgaria", "BA": "Bosnia and Herzegovina", "BB": "Barbados", "WF": "Wallis and Futuna", "BL": "Saint Barthelemy", "BM": "Bermuda", "BN": "Brunei", "BO": "Bolivia", "BH": "Bahrain", "BI": "Burundi", "BJ": "Benin", "BT": "Bhutan", "JM": "Jamaica", "BV": "Bouvet Island", "BW": "Botswana", "WS": "Samoa", "BQ": "Bonaire, Saint Eustatius and Saba ", "BR": "Brazil", "BS": "Bahamas", "JE": "Jersey", "BY": "Belarus", "BZ": "Belize", "RU": "Russia", "RW": "Rwanda", "RS": "Serbia", "TL": "East Timor", "RE": "Reunion", "TM": "Turkmenistan", "TJ": "Tajikistan", "RO": "Romania", "TK": "Tokelau", "GW": "Guinea-Bissau", "GU": "Guam", "GT": "Guatemala", "GS": "South Georgia and the South Sandwich Islands", "GR": "Greece", "GQ": "Equatorial Guinea", "GP": "Guadeloupe", "JP": "Japan", "GY": "Guyana", "GG": "Guernsey", "GF": "French Guiana", "GE": "Georgia", "GD": "Grenada", "GB": "United Kingdom", "GA": "Gabon", "SV": "El Salvador", "GN": "Guinea", "GM": "Gambia", "GL": "Greenland", "GI": "Gibraltar", "GH": "Ghana", "OM": "Oman", "TN": "Tunisia", "JO": "Jordan", "HR": "Croatia", "HT": "Haiti", "HU": "Hungary", "HK": "Hong Kong", "HN": "Honduras", "HM": "Heard Island and McDonald Islands", "VE": "Venezuela", "PR": "Puerto Rico", "PS": "Palestinian Territory", "PW": "Palau", "PT": "Portugal", "SJ": "Svalbard and Jan Mayen", "PY": "Paraguay", "IQ": "Iraq", "PA": "Panama", "PF": "French Polynesia", "PG": "Papua New Guinea", "PE": "Peru", "PK": "Pakistan", "PH": "Philippines", "PN": "Pitcairn", "PL": "Poland", "PM": "Saint Pierre and Miquelon", "ZM": "Zambia", "EH": "Western Sahara", "EE": "Estonia", "EG": "Egypt", "ZA": "South Africa", "EC": "Ecuador", "IT": "Italy", "VN": "Vietnam", "SB": "Solomon Islands", "ET": "Ethiopia", "SO": "Somalia", "ZW": "Zimbabwe", "SA": "Saudi Arabia", "ES": "Spain", "ER": "Eritrea", "ME": "Montenegro", "MD": "Moldova", "MG": "Madagascar", "MF": "Saint Martin", "MA": "Morocco", "MC": "Monaco", "UZ": "Uzbekistan", "MM": "Myanmar", "ML": "Mali", "MO": "Macao", "MN": "Mongolia", "MH": "Marshall Islands", "MK": "Macedonia", "MU": "Mauritius", "MT": "Malta", "MW": "Malawi", "MV": "Maldives", "MQ": "Martinique", "MP": "Northern Mariana Islands", "MS": "Montserrat", "MR": "Mauritania", "IM": "Isle of Man", "UG": "Uganda", "TZ": "Tanzania", "MY": "Malaysia", "MX": "Mexico", "IL": "Israel", "FR": "France", "IO": "British Indian Ocean Territory", "SH": "Saint Helena", "FI": "Finland", "FJ": "Fiji", "FK": "Falkland Islands", "FM": "Micronesia", "FO": "Faroe Islands", "NI": "Nicaragua", "NL": "Netherlands", "NO": "Norway", "NA": "Namibia", "VU": "Vanuatu", "NC": "New Caledonia", "NE": "Niger", "NF": "Norfolk Island", "NG": "Nigeria", "NZ": "New Zealand", "NP": "Nepal", "NR": "Nauru", "NU": "Niue", "CK": "Cook Islands", "XK": "Kosovo", "CI": "Ivory Coast", "CH": "Switzerland", "CO": "Colombia", "CN": "China", "CM": "Cameroon", "CL": "Chile", "CC": "Cocos Islands", "CA": "Canada", "CG": "Republic of the Congo", "CF": "Central African Republic", "CD": "Democratic Republic of the Congo", "CZ": "Czech Republic", "CY": "Cyprus", "CX": "Christmas Island", "CR": "Costa Rica", "CW": "Curacao", "CV": "Cape Verde", "CU": "Cuba", "SZ": "Swaziland", "SY": "Syria", "SX": "Sint Maarten", "KG": "Kyrgyzstan", "KE": "Kenya", "SS": "South Sudan", "SR": "Suriname", "KI": "Kiribati", "KH": "Cambodia", "KN": "Saint Kitts and Nevis", "KM": "Comoros", "ST": "Sao Tome and Principe", "SK": "Slovakia", "KR": "South Korea", "SI": "Slovenia", "KP": "North Korea", "KW": "Kuwait", "SN": "Senegal", "SM": "San Marino", "SL": "Sierra Leone", "SC": "Seychelles", "KZ": "Kazakhstan", "KY": "Cayman Islands", "SG": "Singapore", "SE": "Sweden", "SD": "Sudan", "DO": "Dominican Republic", "DM": "Dominica", "DJ": "Djibouti", "DK": "Denmark", "VG": "British Virgin Islands", "DE": "Germany", "YE": "Yemen", "DZ": "Algeria", "US": "United States", "UY": "Uruguay", "YT": "Mayotte", "UM": "United States Minor Outlying Islands", "LB": "Lebanon", "LC": "Saint Lucia", "LA": "Laos", "TV": "Tuvalu", "TW": "Taiwan", "TT": "Trinidad and Tobago", "TR": "Turkey", "LK": "Sri Lanka", "LI": "Liechtenstein", "LV": "Latvia", "TO": "Tonga", "LT": "Lithuania", "LU": "Luxembourg", "LR": "Liberia", "LS": "Lesotho", "TH": "Thailand", "TF": "French Southern Territories", "TG": "Togo", "TD": "Chad", "TC": "Turks and Caicos Islands", "LY": "Libya", "VA": "Vatican", "VC": "Saint Vincent and the Grenadines", "AE": "United Arab Emirates", "AD": "Andorra", "AG": "Antigua and Barbuda", "AF": "Afghanistan", "AI": "Anguilla", "VI": "U.S. Virgin Islands", "IS": "Iceland", "IR": "Iran", "AM": "Armenia", "AL": "Albania", "AO": "Angola", "AQ": "Antarctica", "AS": "American Samoa", "AR": "Argentina", "AU": "Australia", "AT": "Austria", "AW": "Aruba", "IN": "India", "AX": "Aland Islands", "AZ": "Azerbaijan", "IE": "Ireland", "ID": "Indonesia", "UA": "Ukraine", "QA": "Qatar", "MZ": "Mozambique"}
+
+function parseLocation(raw) {
+    let parsed = ''
+    if (!raw) {
+        return ''
+    }
+    const ALLOWED_CHARS = 'abcdefghijklmnopqrstuvwxyz'
+    const cleanedResponse = raw.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase()
+    for (let i=0; i<cleanedResponse.length; i++) {
+        const c = cleanedResponse[i]
+        if (ALLOWED_CHARS.includes(c)) {
+            parsed += c
+        }
+    }
+    if (parsed.length < 3) {
+        return ''
+    }
+    return parsed
+}
+
 // Snoop user city from IP in order to provide creepy location for spirit.
 // Using free CloudFlare worker to bypass Adblockers.
 // Geolocation provider is CloudFlare, no other third parties involved.
 let userCity = ''
+let userRegion = ''
+let userCountry = ''
 fetch('https://geo.atte-cloudflare.workers.dev/', { mode: 'cors' })
     .then(res => res.json())
     .then(response => {
-        const ALLOWED_CHARS = 'abcdefghijklmnopqrstuvwxyz'
-        const cleanedResponse = response.city.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase()
-        for (let i=0; i<cleanedResponse.length; i++) {
-            const c = cleanedResponse[i]
-            if (ALLOWED_CHARS.includes(c)) {
-                userCity += c
-            }
-        }
-        if (userCity.length < 3) {
-            userCity = ''
-        }
+        userCity = parseLocation(response.city)
+        userRegion = parseLocation(response.region)
+        userCountry = countryMappings[response.country] || ''
     })
     .catch(exception => {
         // Ad-blocker prevents request? Rate limited? Suppress error from user.
@@ -219,6 +234,30 @@ const scriptedExperience = [
         ],
     },
     {
+        trigger: /^country$/,
+        options: [
+            { value: '!COUNTRY' },
+        ],
+    },
+    {
+        trigger: /^(which|what) country.*/,
+        options: [
+            { value: '!COUNTRY' },
+        ],
+    },
+    {
+        trigger: /^(state|county|region)$/,
+        options: [
+            { value: '!REGION' },
+        ],
+    },
+    {
+        trigger: /^(which|what) (state|county|region).*/,
+        options: [
+            { value: '!REGION' },
+        ],
+    },
+    {
         trigger: /.*(^| )(bitch|asshole|jerk|harlot|idiot|stupid|faggot|gay|dickhead|suck|sucker|cocksucker|retard|fuck|fucking|shit|shut up|fucker|motherfucker|liar|whore|dumb|dumbass)($| ).*/,
         options: [
             { value: '!INSULTED' },
@@ -269,6 +308,7 @@ const scriptedExperience = [
             { value: 'nohope' },
             { value: 'nope' },
             { value: 'never' },
+            { value: 'sorryno', restrictedTo: [FRIENDLY] },
         ],
     },
     {
@@ -1306,6 +1346,12 @@ const resolveQueryWithSimpleChatbot = function(query) {
         if (questGoals.where === 2) return resolveQueryWithSimpleChatbot(`{location2}`)
         if (questGoals.where === 1) return resolveQueryWithSimpleChatbot(`{location1}`)
         return resolveQueryWithSimpleChatbot('{location0}')
+    }
+    if (query.startsWith('!COUNTRY')) {
+        return userCountry || resolveQueryWithSimpleChatbot('{location0}')
+    }
+    if (query.startsWith('!REGION')) {
+        return userRegion || resolveQueryWithSimpleChatbot('{location0}')
     }
     if (query.startsWith('!DEFINE')) {
         scriptedExperience.forEach((node) => node.options = node.options.filter((option) => !option.value.startsWith('!DEFINE')))
